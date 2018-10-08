fdf4c6987896a39bf91d3c8671ce68dc8cb1b74f
emmeiwhite <emmeiwhite@gmail.com>
2018-10-08 18:52:52 +0530

    Conditional Data Rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Counter from './components/counter';
 class App extends React.Component{
     render(){
         return(
-               <Counter/>
+               <Counter />
         );
     }
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -10,7 +10,7 @@ class Counter extends Component {
         count:0,
         imgPerson:'https://picsum.photos/200', // getting from the server
         localImage:'./images/person8',
-        altText:"UnAble to load the local image"
+        lists:['tag1','tag2','tag3']
     }
 
     styles = {
@@ -24,16 +24,28 @@ class Counter extends Component {
                 <img src={this.state.imgPerson} alt="Not Loading, Not Loading"/>
                 <img src={person} style={this.styles}/>
                 <button onClick={this.onClickHandler} className="btn btn-secondary btn-sm">Increment</button>
+                {/* Rendering Lists: */}
+                <h1>Rendering Lists</h1> <button onClick={this.makeListsEmpty}>{this.state.lists.length===0?"SHOW LISTS":"Here is the list"}</button>
+                {this.renderTags()}
+
+                {/* true && 'hello where is JSX' return trusy statement, the last truesy statement*/}
+
+                {
+                    this.state.count===10 && (<h2>Rendering Content Conditionally using and operator</h2>)
+                }
+              
+
+               
             </React.Fragment>);
     }
 
-    newMethod() {
+    newMethod=()=> {
         let classes = "badge  m-2 badge-";
         classes += this.state.count === 0 ? 'warning' : 'danger';
         return classes;
     }
 
-    formatCount(){
+    formatCount=()=>{
         const {count} = this.state; // Object Destructuring
 
         return count===0?"ZERO":count;
@@ -41,11 +53,27 @@ class Counter extends Component {
     }
 
     onClickHandler=()=>{
-        const {count} = this.state;
-
-        const newState = {...this.state};//We never mutate the state
+        const newState = {...this.state};//We never mutate the state, Here we have copied the state
         newState.count=10;
         this.setState(newState); 
+    }
+
+    renderTags=()=>{
+        if(this.state.lists.length===0) return <h2>There are no list of tags to display !!!</h2>
+
+        return (
+            <ul className="list-group">
+                {this.state.lists.map((tag,index)=><li key={index} className="list-group-item">{tag}</li>)} {/*Returning in one shot*/}
+            </ul>
+        )
+    }
+
+    makeListsEmpty=()=>{
+        const modifiedState = {...this.state};
+        modifiedState.lists = [];
+        this.setState(
+           modifiedState
+        )
     }
 }
  
